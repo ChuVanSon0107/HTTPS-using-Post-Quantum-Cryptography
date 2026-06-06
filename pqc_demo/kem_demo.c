dd9f030939d33f3c1072a5cbb19e390d27a4a23e
ChuVanSon0107 <cvson1126034@gmail.com>
2026-06-06 10:22:22 +0700

    Add initial implementation files for PQC demo

---
+++ pqc_demo/kem_demo.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include <oqs/oqs.h>
+
+void cleanup_heap(uint8_t *secret_key, uint8_t *shared_secret_e,
+                  uint8_t *shared_secret_d, uint8_t *public_key,
+                  uint8_t *ciphertext, OQS_KEM *kem);
+
+int main() {
+	OQS_KEM *kem = NULL;
+	uint8_t *public_key = NULL;
+	uint8_t *secret_key = NULL;
+	uint8_t *ciphertext = NULL;
+	uint8_t *shared_secret_e = NULL;
+	uint8_t *shared_secret_d = NULL;
+
+    kem = OQS_KEM_new(OQS_KEM_alg_ml_kem_768);
+	if (kem == NULL) {
+		printf("Cannot initialize ML-KEM-768\n");
+		return OQS_ERROR;
+	}
+
+    printf("Algorithm: %s\n", kem->method_name);
+
+    public_key = OQS_MEM_malloc(kem->length_public_key);
+	secret_key = OQS_MEM_malloc(kem->length_secret_key);
+	ciphertext = OQS_MEM_malloc(kem->length_ciphertext);
+	shared_secret_e = OQS_MEM_malloc(kem->length_shared_secret);
+	shared_secret_d = OQS_MEM_malloc(kem->length_shared_secret);
+
+	if ((public_key == NULL) || (secret_key == NULL) || (ciphertext == NULL) ||
+	        (shared_secret_e == NULL) || (shared_secret_d == NULL)) {
+		fprintf(stderr, "ERROR: OQS_MEM_malloc failed!\n");
+		cleanup_heap(secret_key, shared_secret_e, shared_secret_d, public_key,
+		             ciphertext, kem);
+
+		return OQS_ERROR;
+	}
+
+    printf("\n[1] Generating keypair...\n");
+	OQS_STATUS rc = OQS_KEM_keypair(kem, public_key, secret_key);
+    if (rc != OQS_SUCCESS) {
+ 		fprintf(stderr, "ERROR: OQS_KEM_encaps failed!\n");
+		cleanup_heap(secret_key, shared_secret_e, shared_secret_d, public_key,
+		             ciphertext, kem);
+
+		return OQS_ERROR;
+    }
+
+    printf("[OK] Keypair generated\n");
+
+    printf("\n[2] Encapsulating...\n");
+    rc = OQS_KEM_encaps(kem, ciphertext, shared_secret_e, public_key);
+    if (rc != OQS_SUCCESS) {
+		fprintf(stderr, "ERROR: OQS_KEM_encaps failed!\n");
+		cleanup_heap(secret_key, shared_secret_e, shared_secret_d, public_key,
+		             ciphertext, kem);
+
+		return OQS_ERROR;
+    }
+
+    printf("[OK] Encapsulation completed\n");
+
+    printf("\n[3] Decapsulating...\n");
+    rc = OQS_KEM_decaps(kem, shared_secret_d, ciphertext, secret_key);
+    if (rc != OQS_SUCCESS) {
+		fprintf(stderr, "ERROR: OQS_KEM_decaps failed!\n");
+		cleanup_heap(secret_key, shared_secret_e, shared_secret_d, public_key,
+		             ciphertext, kem);
+
+		return OQS_ERROR;
+    }
+
+    printf("[OK] Decapsulation completed\n");
+
+    printf("\n[4] Comparing shared secrets...\n");
+
+    if (memcmp(shared_secret_e, shared_secret_d, kem->length_shared_secret) == 0) {
+        printf("SUCCESS: Shared Secret Match!\n");
+    } else {
+        printf("FAILED: Secrets do not match!\n");
+    }
+
+    cleanup_heap(secret_key, shared_secret_e, shared_secret_d, public_key, ciphertext, kem);
+    
+    return OQS_SUCCESS;
+}
+
+void cleanup_heap(uint8_t *secret_key, uint8_t *shared_secret_e,
+                  uint8_t *shared_secret_d, uint8_t *public_key,
+                  uint8_t *ciphertext, OQS_KEM *kem) {
+	if (kem != NULL) {
+		OQS_MEM_secure_free(secret_key, kem->length_secret_key);
+		OQS_MEM_secure_free(shared_secret_e, kem->length_shared_secret);
+		OQS_MEM_secure_free(shared_secret_d, kem->length_shared_secret);
+	}
+	OQS_MEM_insecure_free(public_key);
+	OQS_MEM_insecure_free(ciphertext);
+	OQS_KEM_free(kem);
+}
